71e6bd9645db5e6c289ec64ad57d7c33ac94a231
Alcaide F. Inocente <ialcaidef@hotmail.com>
2020-09-27 19:35:42 +0200

    Add project files.

---
+++ ContosoConf/scripts/pages/register.js
@@ -0,0 +1,33 @@
+﻿// Get the registration <form> element from the DOM.
+const form = document.getElementById("registration-form");
+const submitButton = form.querySelector("button");
+// Get the <input> elements from the DOM.
+const passwordInput = document.getElementById("password");
+const confirmPasswordInput = document.getElementById("confirm-password");
+
+const checkPasswords = () => {
+    const passwordsMatch = passwordInput.value === confirmPasswordInput.value;
+    if (passwordsMatch) {
+        // Clear any previous error message.
+        confirmPasswordInput.setCustomValidity("");
+    } else {
+        // Setting this error message will prevent the form from being submitted.
+        confirmPasswordInput.setCustomValidity("Your passwords don't match. Please type the same password again.");
+    }
+};
+
+const addPasswordInputEventListeners = () => {
+    passwordInput.addEventListener("input", checkPasswords, false);
+    confirmPasswordInput.addEventListener("input", checkPasswords, false);
+};
+
+const formSubmissionAttempted = () => {
+    form.classList.add("submission-attempted");
+};
+
+const addSubmitClickEventListener = () => {
+    submitButton.addEventListener("click", formSubmissionAttempted, false);
+};
+
+addPasswordInputEventListeners();
+addSubmitClickEventListener();
---
+++ ContosoConf/scripts/pages/schedule.js
@@ -0,0 +1,102 @@
+﻿const schedules = [];
+const list = document.getElementById("schedule");
+const track1CheckBox = document.getElementById("show-track-1");
+const track2CheckBox = document.getElementById("show-track-2");
+
+const downloadSchedule = async () => {
+
+    // await response of fetch call
+    let response = await fetch("/schedule/list");
+    // transform body to json
+    let data = await response.json();
+
+    // checking response is ok
+    if (response.ok) {
+        schedules = data.schedule;
+        displaySchedule();
+    }
+    else
+        alert("Schedule list not available.");
+}
+
+const createSessionElement = (session) => {
+    const li = document.createElement("li");
+
+    li.sessionId = session.id;
+
+    const star = document.createElement("a");
+    star.setAttribute("href", "#");
+    star.setAttribute("class", "star");
+    li.appendChild(star);
+
+    const title = document.createElement("span");
+    title.textContent = session.title;
+    li.appendChild(title);
+
+    return li;
+};
+
+const clearList = () => {
+    while (list.firstChild) {
+        list.removeChild(list.firstChild);
+    }
+}
+
+const displaySchedule = () => {
+    clearList();
+    for (let schedule of schedules) {
+        const tracks = schedule.tracks;
+        const isCurrentTrack = (track1CheckBox.checked && tracks.indexOf(1) >= 0) ||
+            (track2CheckBox.checked && tracks.indexOf(2) >= 0);
+        if (isCurrentTrack) {
+            const li = createSessionElement(schedule);
+            list.appendChild(li);
+        }
+    }
+}
+
+const saveStar = async (sessionId, isStarred) => {
+
+    const headers = new Headers({
+        "Content-Type": "application/x-www-form-urlencoded"
+    })
+
+
+    const options = {
+        method: 'POST',
+        headers: headers,
+        body: "starred=" + isStarred
+    }
+
+    const response = await fetch("/schedule/star/" + sessionId, options);
+
+    if (isStarred) {
+        if (response.ok) {
+            const data = await response.json();
+            if (data.starCount > 50)
+                alert("This session is very popular! Be sure to arrive early to get a seat.");
+        }
+    }
+}
+
+const handleListClick = async (event) => {
+    const isStarElement = event.srcElement.classList.contains("star");
+    if (isStarElement) {
+        event.preventDefault(); // Stop the browser following the clicked <a> element's href.
+
+        const listItem = event.srcElement.parentNode;
+        if (listItem.classList.contains("starred")) {
+            listItem.classList.remove("starred");
+            await saveStar(listItem.sessionId, false);
+        } else {
+            listItem.classList.add("starred");
+            await saveStar(listItem.sessionId, true);
+        }
+    }
+}
+
+track1CheckBox.addEventListener("click", displaySchedule, false);
+track2CheckBox.addEventListener("click", displaySchedule, false);
+list.addEventListener("click", handleListClick, false);
+
+downloadSchedule();
